3b2ec3effd2ccb1518308dcdc62bf7772796f95b
Kendall Poindexter <kendall.poindexter@detroitlabs.com>
2018-10-08 16:06:26 -0400

    Added dogdetail swift file and connected to view

---
+++ Project-Lily/Project-Lily/DogDetailViewController.swift
@@ -0,0 +1,34 @@
+//
+//  DogDetailViewController.swift
+//  Project-Lily
+//
+//  Created by Kendall Poindexter on 10/8/18.
+//  Copyright © 2018 Kendall Poindexter. All rights reserved.
+//
+
+import UIKit
+
+class DogDetailViewController: UIViewController {
+    
+    //MARK: - Outlets
+    
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
