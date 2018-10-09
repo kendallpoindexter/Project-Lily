add535176b3747cac0ad23bf4efa55e0c35fc1d3
Kendall Poindexter <kendall.poindexter@detroitlabs.com>
2018-10-09 13:49:34 -0400

    Added Dog class

---
+++ Project-Lily/Project-Lily/DogClass.swift
@@ -0,0 +1,19 @@
+//
+//  DogClass.swift
+//  Project-Lily
+//
+//  Created by Kendall Poindexter on 10/9/18.
+//  Copyright © 2018 Kendall Poindexter. All rights reserved.
+//
+
+import Foundation
+
+class Dog {
+    let id: Int?
+    let name: String?
+    
+    init(id: Int, name: String) {
+        self.id = id
+        self.name = name
+    }
+}
