017b8b2e4398c4d9e7758ba463b4fe203d595719
Kendall Poindexter <kendall.poindexter@detroitlabs.com>
2018-10-22 10:40:35 -0400

    Connected favorites button to DogDetailViewController

--- Project-Lily/Project-Lily/DogDetailViewController.swift
+++ Project-Lily/Project-Lily/DogDetailViewController.swift
@@ -57,14 +57,11 @@ class DogDetailViewController: UIViewController {
         
     }
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    //MARK: - Actions
+    
+    @IBAction func favoriteTapped(_ sender: UIButton) {
+        
     }
-    */
+    
 
 }
