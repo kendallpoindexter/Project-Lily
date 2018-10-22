7e9160d5458ab5e1036d96b988370734dd74fc6a
Kendall Poindexter <kendall.poindexter@detroitlabs.com>
2018-10-22 14:30:01 -0400

    Created custom cell file

---
+++ Project-Lily/Project-Lily/FavoriteDogCell.swift
@@ -0,0 +1,13 @@
+//
+//  FavoriteDogCell.swift
+//  Project-Lily
+//
+//  Created by Kendall Poindexter on 10/22/18.
+//  Copyright © 2018 Kendall Poindexter. All rights reserved.
+//
+
+import UIKit
+
+class FavoriteDogImageCell: UICollectionViewCell {
+    
+}
