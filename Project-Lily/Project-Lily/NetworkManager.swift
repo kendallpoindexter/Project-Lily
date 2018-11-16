cd273027192dda1efe1da021a15a0afde272a57b
Kendall Poindexter <kendall.poindexter@detroitlabs.com>
2018-11-16 13:34:11 -0500

    Created Networking layer within project folder and created network manager class

---
+++ Project-Lily/Project-Lily/NetworkManager.swift
@@ -0,0 +1,13 @@
+//
+//  NetworkManager.swift
+//  Project-Lily
+//
+//  Created by Kendall Poindexter on 11/16/18.
+//  Copyright © 2018 Kendall Poindexter. All rights reserved.
+//
+
+import Foundation
+
+class NetworkManager {
+    
+}
